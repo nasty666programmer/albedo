75da9ebb678d7037d3946d31742ef440674b6bb3
nasty666programmer <ivanov1study@gmail.com>
2020-04-23 19:26:00 +0300

    add JQUery

--- src/public/javascript/maps.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-
-    var uluru = {lat: 34.1010486, lng: -118.3439089};
-    
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 18, center: uluru});
-    
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  }
